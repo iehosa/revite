aace60dba7b2213a661aa590e37d959a111d3aa3
i Yeong <gyu@iui-MacBookAir.local>
2024-10-19 16:50:11 +0900

    why

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,7 @@
 /** @format */
 
-/*
-
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  base: "/vitestt/",
-  build: {
-    assetsDir: "static",
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split(".").at(1);
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = "img";
-          }
-          return `assets/${extType}/[name]-[hash][extname]`;
-        },
-        chunkFileNames: "assets/js/[name]-[hash].js",
-        entryFileNames: "assets/js/[name]-[hash].js",
-      },
-    },
-  },
-
-  resolve: {
-    alias: [{ find: "@lib", replacement: "./lib" }, { find: "@data", replacement: "./data" }, { find: "@components", replacement: "./components" }, { "@": Path2D.resolve(__dirname, "src") }],
-  },
-});
-
-*/
-
 export default {
-  base: "/vitestt/",
+  base: "/revite/",
   build: {
     cssMinify: false,
     rollupOptions: {
